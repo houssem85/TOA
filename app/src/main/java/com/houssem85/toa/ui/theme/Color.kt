0a37e45b36df97fad2c478ecb1f29994c1c3f1b5
DESKTOP-UMLT1T9\android <h.eddine-daoued@oxton-digital.com>
2022-02-06 10:18:26 +0100

    Removing unused color variables

--- app/src/main/java/com/houssem85/toa/ui/theme/Color.kt
+++ app/src/main/java/com/houssem85/toa/ui/theme/Color.kt
@@ -4,11 +4,6 @@ package com.houssem85.toa.ui.theme
 import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
 private val PrimaryBlue = Color(0xFF0A3751)
 private val SecondaryOrange = Color(0xFFFC8138)
 private val BackgroundBlue = Color(0xFFF4F4F8)
