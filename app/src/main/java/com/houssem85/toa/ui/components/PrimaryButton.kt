a0d10057bf2c3c4e77d99ad8408a57ca75d0ec18
DESKTOP-UMLT1T9\android <h.eddine-daoued@oxton-digital.com>
2022-02-06 11:32:05 +0100

    Adding PrimaryButton for #34

---
+++ app/src/main/java/com/houssem85/toa/ui/components/PrimaryButton.kt
@@ -0,0 +1,59 @@
+package com.houssem85.toa.ui.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults.buttonColors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.toUpperCase
+import androidx.compose.ui.tooling.preview.Preview
+import com.houssem85.toa.R
+import com.houssem85.toa.ui.theme.TOATheme
+
+@Composable
+fun PrimaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colors.primary,
+) {
+    val buttonColors = buttonColors(
+        backgroundColor = color
+    )
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .height(dimensionResource(id = R.dimen.button_height))
+            .fillMaxHeight(),
+        colors = buttonColors,
+        shape = RoundedCornerShape(50),
+    ) {
+        Text(
+            text = text.toUpperCase(Locale.current)
+        )
+    }
+}
+
+@Preview(
+    name = "Night Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Preview(
+    name = "Day Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+)
+@Composable
+@Suppress("UnusedPrivateMember")
+private fun PrimaryButtonPreview() {
+    TOATheme {
+        PrimaryButton("hello in jetpak",{})
+    }
+}
